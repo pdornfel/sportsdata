a59132f9a5a6f9014c8ee1672eb1e93a4000da55
Aldo Delgado <aldo@osstek.com>
2013-10-23 14:47:55 -0400

    Fixed methods for mlb when pulling from sportsdata

--- lib/sportsdata/mlb.rb
+++ lib/sportsdata/mlb.rb
@@ -28,100 +28,111 @@ module Sportsdata
     def self.venues(options = {})
       venues = []
       response = self.get_raw(self.venues_url)
-      all_venues = response['league'].try(:[], 'conference')
+      all_venues = response['venues'].try(:[], 'venue')
       all_venues ||= []
-      all_venues.each { |conference|
-        conference['division'].each { |division|
-          division['team'].each { |team|
-            venue_record = {}
-            venue_record[:name]       = team['venue']['name'].humanize.titlecase
-            venue_record[:state]      = team['venue']['state']
-            venue_record[:city]       = team['venue']['city']
-            venue_record[:guid]       = team['venue']['id']
-            venue_record[:capacity]   = team['venue']['capacity']
-            venue_record[:surface]    = team['venue']['surface']
-            venue_record[:venue_type] = team['venue']['type']
-            venues.append(Venue.new(venue_record))
-          }
-        }
+      all_venues.each { |venue|
+        venue_record = {}
+        venue_record[:guid]                       = venue['id']
+        venue_record[:name]                       = venue['name']
+        venue_record[:city]                       = venue['market']
+        venue_record[:left_field]                 = venue['distances']['lf']
+        venue_record[:left_center_field]          = venue['distances']['lcf']
+        venue_record[:center_field]               = venue['distances']['cf']
+        venue_record[:right_center_field]         = venue['distances']['rcf']
+        venue_record[:right_field]                = venue['distances']['rf']
+        venue_record[:middle_left_field]          = venue['distances']['mlf']
+        venue_record[:middle_left_center__field]  = venue['distances']['mlcf']
+        venue_record[:middle_right_center_field]  = venue['distances']['mrcf']
+        venue_record[:middle_right_field]         = venue['distances']['mrf']
+        venues.append(Venue.new(venue_record))
       }
       venues
     end
 
-    def self.teams(options = {})
+    def self.teams(options = {:year => Date.today.year})
       teams = []
-      response = self.get_raw(self.teams_url)
-      all_teams = response['league'].try(:[], 'conference')
+      response = self.get_raw(self.teams_url(:year => 2013))
+      all_teams = response['teams'].try(:[], 'team')
       all_teams ||= []
-      all_teams.each { |conference|
+      all_teams.each { |team|
         team_record = {}
-        team_record[:league_abbr] = conference['name']
-        conference['division'].each { |division|
-          team_record[:division] = division['name']
-          division['team'].each { |team|
-            team_record[:guid]  = team['id']
-            team_record[:abbr]  = team['id']
-            team_record[:name]  = team['market'] + ' ' + team['name']
-            team_record[:slug]  = sd.to_slug(team_record['name']).downcase
-            team_record[:city]  = team['market']
-            teams.append(Team.new(team_record))
-          }
-        }
+        team_record[:guid]        = team['id']
+        team_record[:abbr]        = team['abbr']
+        team_record[:name]        = team['name']
+        team_record[:city]        = team['market']
+        team_record[:league]      = team['league']
+        team_record[:division]    = team['division']
+        team_record[:venue_guid]  = team['venue']
+        teams.append(Team.new(team_record))
       }
       teams
     end
 
-    #fetch last year, this year and next year
+    # Fetch last year, this year and next year
     # Their are three season options (PRE, REG, PST)
-    def self.games(options = {:year => Date.today.year, :season => 'REG'})
+    def self.games(options = {:year => Date.today.year})
       games = []
       response = self.get_raw(games_url(:year => 2012))
       #games_url(:year => 2012)
       #games_url(:year => 2012)
-      all_games = response['season'].try(:[], 'week')
+      all_games = response['calendars'].try(:[], 'event')
       all_games ||= []
-      all_games.each { |week|
+      all_games.each { |game|
         game_record = {}
-        game_record[:week]  = week['week']
-        week['game'].each { |game|
-          game_record[:guid]      = game['guid']
-          game_record[:scheduled] = game['scheduled']
-          game_record[:home]      = game['home']
-          game_record[:away]      = game['away']
-          game_record[:status]    = game['status']
-          games.append(Game.new(game_record))
-        }
+        game_record[:guid]                = game['id']
+        game_record[:scheduled_start]     = game['scheduled_start']
+        game_record[:season_type]         = game['season_type']
+        game_record[:status]              = game['status']
+        game_record[:visitor]             = game['visitor']
+        game_record[:home]                = game['home']
+        game_record[:venue]               = game['venue']
+        game_record[:tbd]                 = game['tbd']
+        game_record[:broadcast_network]   = game['broadcast']['network']
+        game_record[:broadcast_satellite] = game['broadcast']['satellite']
+        game_record[:broadcast_internet]  = game['broadcast']['internet']
+        game_record[:broadcast_cable]     = game['broadcast']['cable']
+        games.append(Game.new(game_record))
       }
       games
     end
 
-    def self.players(options = {})
+    def self.players(options = {:year => Date.today.year})
       players = []
-      response = self.get_raw(players_url(:team_abbr => 'MIA'))
-      all_players = response['team'].try(:[], 'player')
+      response = self.get_raw(players_url(:year => '2013'))
+      all_players = response['rosters'].try(:[], 'team')
       all_players ||= []
-      all_players.each { |player|
-        player_record = {}
-        player_record[:guid]            = player['id']
-        player_record[:name_full]       = player['name_full']
-        player_record[:name_first]      = player['name_first']
-        player_record[:name_last]       = player['name_last']
-        player_record[:name_abbr]       = player['name_abbr']
-        player_record[:birthdate]       = player['birthdate']
-        player_record[:birth_place]     = player['birth_place']
-        player_record[:high_school]     = player['high_school']
-        player_record[:height]          = player['height']
-        player_record[:weight]          = player['weight']
-        player_record[:college]         = player['college']
-        player_record[:position]        = player['position']
-        player_record[:jersey_number]   = player['jersey_number']
-        player_record[:status]          = player['status']
-        player_record[:salary]          = player['salary']
-        player_record[:experience]      = player['experience']
-        player_record[:draft_pick]      = player['draft_pick']
-        player_record[:draft_round]     = player['draft_round']
-        player_record[:draft_team]      = player['draft_team']
-        players.append(Player.new(player_record))
+      all_players.each { |team|
+        if team['players']
+          player_record = {}
+          player_record[:team_guid]       = team['id']
+          player_record[:team_abbr]       = team['abbr']
+          player_record[:team_name]       = team['name']
+          player_record[:team_market]     = team['market']
+          player_record[:league]          = team['league']
+          player_record[:division]        = team['division']
+          team['players']['profile'].each { |player|
+            player_record[:player_guid]     = player['id']
+            player_record[:mlbam_id]        = player['mlbam_id']
+            player_record[:first_name]      = player['first']
+            player_record[:preferred_first] = player['preferred_first']
+            player_record[:last]            = player['last']
+            player_record[:bat_hand]        = player['bat_hand']
+            player_record[:throw_hand]      = player['throw_hand']
+            player_record[:weight]          = player['weight']
+            player_record[:height]          = player['height']
+            player_record[:birthdate]       = player['birthdate']
+            player_record[:birthcity]       = player['birthcity']
+            player_record[:birthstate]      = player['birthstate']
+            player_record[:birthcountry]    = player['birthcountry']
+            player_record[:highschool]      = player['highschool']
+            player_record[:college]         = player['college']
+            player_record[:pro_debut]       = player['pro_debut']
+            player_record[:status]          = player['status']
+            player_record[:jersey]          = player['jersey']
+            player_record[:position]        = player['position']
+            players.append(Player.new(player_record))
+          }
+        end
       }
       players
     end
@@ -136,19 +147,19 @@ module Sportsdata
     end
 
     def self.venues_url
-      "teams/hierarchy.xml"
+      "venues/venues.xml"
     end
 
-    def self.teams_url
-      "teams/hierarchy.xml"
+    def self.teams_url(options = {})
+      "teams/#{options[:year]}.xml"
     end
 
     def self.games_url(options = {})
-      "#{options[:year]}/#{options[:season]}/schedule.xml"
+      "schedule/#{options[:year]}.xml"
     end
 
     def self.players_url(options = {})
-      "teams/#{options[:team_abbr]}/roster.xml"
+      "rosters-full/#{options[:year]}.xml"
     end
 
     def self.api
