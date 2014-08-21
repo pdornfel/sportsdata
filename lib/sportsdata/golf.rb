e0db512214255790f1b1ca8b2094328ac81357cb
Paul Dornfeld <pdornfel@gmail.com>
2014-08-21 11:22:29 -0400

    created get seasonal stats. removed code copied and pasted from other classes

--- lib/sportsdata/golf.rb
+++ lib/sportsdata/golf.rb
@@ -3,15 +3,9 @@ module Sportsdata
     include Request
     class Exception < ::Exception
     end
-    class Venue < OpenStruct
+    class Stat < OpenStruct
     end
     class Player < OpenStruct
-    end
-    class Team < OpenStruct
-    end
-    class Schedule < OpenStruct
-    end
-    class Game < OpenStruct
     end
 
     attr_accessor :api_key, :api_mode
@@ -33,116 +27,18 @@ module Sportsdata
       'golf'
     end
 
-    def self.player_statuses
-      [
-        ["CUT"],
-        ["WD"]
-      ]
-    end
-
-    def self.tournament_statuses
-      [
-        ["scheduled - The tournament is scheduled to occur."],
-        ["inprogress – The tournament is in progress."],
-        ["complete – The tournament is over, but stat validation is not complete."],
-        ["closed – The tournament is over and the stats have been validated."],
-        ["reopened – The tournament stats are in the process of being corrected."]
-      ]
-    end
-
-    def self.round_statuses
-      [
-        ["scheduled - The round is scheduled to occur."],
-        ["delayed – The round has been delayed."],
-        ["inprogress – The round is in progress."],
-        ["complete – The round is over, but stat validation is not complete."],
-        ["closed – The round is over and the stats have been validated."],
-        ["reopened – The round stats are in the process of being corrected."]
-      ]
-    end
-
-    #def self.venues(options = {})
-    #  venues = []
-    #  response = self.get(self.venues_url)
-    #  all_venues = response['league'].try(:[], 'conference')
-    #  all_venues ||= []
-    #  all_venues.each { |conference|
-    #    conference['division'].each { |division|
-    #      division['team'].each { |team|
-    #        venue_record = {}
-    #        venue_record[:name]       = team['venue']['name'].humanize.titlecase
-    #        venue_record[:state]      = team['venue']['state']
-    #        venue_record[:city]       = team['venue']['city']
-    #        venue_record[:guid]       = team['venue']['id']
-    #        venue_record[:capacity]   = team['venue']['capacity']
-    #        venue_record[:surface]    = team['venue']['surface']
-    #        venue_record[:venue_type] = team['venue']['type']
-    #        venues.append(Venue.new(venue_record))
-    #      }
-    #    }
-    #  }
-    #  venues
-    #end
-
-    #def self.teams(options = {})
-    #  teams = []
-    #  response = self.get(self.teams_url)
-    #  all_teams = response['league'].try(:[], 'conference')
-    #  all_teams ||= []
-    #  all_teams.each { |conference|
-    #    team_record = {}
-    #    team_record[:league_abbr] = conference['name']
-    #    conference['division'].each { |division|
-    #      team_record[:division] = division['name']
-    #      division['team'].each { |team|
-    #        team_record[:guid]  = team['id']
-    #        team_record[:abbr]  = team['id']
-    #        team_record[:name]  = team['market'] + ' ' + team['name']
-    #        team_record[:slug]  = sd.to_slug(team_record['name']).downcase
-    #        team_record[:city]  = team['market']
-    #        teams.append(Team.new(team_record))
-    #      }
-    #    }
-    #  }
-    #  teams
-    #end
-
-    ##fetch last year, this year and next year
-    ## Their are three season options (PRE, REG, PST)
-    #def self.games(options = {:year => Date.today.year, :season => 'REG'})
-    #  games = []
-    #  response = self.get(games_url(:year => options[:year]))
-    #  #games_url(:year => 2012)
-    #  #games_url(:year => 2012)
-    #  all_games = response['season'].try(:[], 'week')
-    #  all_games ||= []
-    #  all_games.each { |week|
-    #    game_record = {}
-    #    game_record[:week]  = week['week']
-    #    week['game'].each { |game|
-    #      game_record[:guid]      = game['guid']
-    #      game_record[:scheduled_at] = game['scheduled']
-    #      game_record[:home]      = game['home']
-    #      game_record[:away]      = game['away']
-    #      game_record[:status]    = game['status']
-    #      games.append(Game.new(game_record))
-    #    }
-    #  }
-    #  games
-    #end
-
-    def self.players(options = {})
+    def self.player_profiles(options = {})
      players = []
-     response = self.get(players_url(:year => '2014'))
+     response = self.get(player_profiles_url(:year => '2014'))
      all_players = response['tour']['season']['player']
      all_players ||= []
-     all_players.each { |player|
+     all_players.each do |player|
        player_record = {}
-       player_record[:id]            = player['id']
+       player_record[:uid]            = player['id']
        player_record[:first_name]    = player['first_name']
        player_record[:last_name]     = player['last_name']
-       player_record[:height]        = player['height']
-       player_record[:weight]        = player['weight']
+       player_record[:height]        = player['height'].to_i
+       player_record[:weight]        = player['weight'].to_i
        player_record[:birthday]      = player['birthday']
        player_record[:country]       = player['country']
        player_record[:birth_place]   = player['birth_place']
@@ -150,24 +46,45 @@ module Sportsdata
        player_record[:updated]       = player['updated']
 
        players.append(Player.new(player_record))
-     }
+     end
      players
     end
 
-    #private
-    #def self.venues_url
-    #  "teams/hierarchy.xml"
-    #end
+    def self.seasonal_stats(options = {})
+     stats = []
+     response = self.get(self.seasonal_stats_url(year: '2014'))
+     seasonal_stats = response['tour']['season']['player']
+     seasonal_stats ||= []
+     seasonal_stats.each do |stat|
+       hash = {}
+       hash[:player_uid]      = stat['id']
+       hash[:first_name]      = stat['first_name']
+       hash[:last_name]       = stat['last_name']
+       hash[:events_played]   = stat['statistics']['events_played'].to_i
+       hash[:first_place]     = stat['statistics']['first_place'].to_i
+       hash[:second_place]    = stat['statistics']['second_place'].to_i
+       hash[:third_place]     = stat['statistics']['third_place'].to_i
+       hash[:top_10]          = stat['statistics']['top_10'].to_i
+       hash[:top_25]          = stat['statistics']['top_25'].to_i
+       hash[:withdrawals]     = stat['statistics']['withdrawals'].to_i
+       hash[:earnings]        = stat['statistics']['earnings'].to_i
+       hash[:earnings_rank]   = stat['statistics']['earnings_rank'].to_i
+       hash[:drive_avg]       = stat['statistics']['drive_avg'].to_i
+       hash[:drive_acc]       = stat['statistics']['drive_acc'].to_i
+       hash[:gir_pct]         = stat['statistics']['gir_pct'].to_i
+       hash[:world_rank]      = stat['statistics']['world_rank'].to_i
+       hash[:scoring_avg]     = stat['statistics']['scoring_avg'].to_i
+           
+       stats.append(Stat.new(hash))
+     end
+     stats
+    end
 
-    #def self.teams_url
-    #  "teams/hierarchy.xml"
-    #end
+    def self.seasonal_stats_url(options = {})
+     "#{self.base_url}/seasontd/pga/#{options[:year]}/players/statistics.xml"
+    end
 
-    #def self.games_url(options = {})
-    #  "#{options[:year]}/#{options[:season]}/schedule.xml"
-    #end
-
-    def self.players_url(options = {})
+    def self.player_profiles_url(options = {})
      "#{self.base_url}/profiles/pga/#{options[:year]}/players/profiles.xml" 
     end
   end
